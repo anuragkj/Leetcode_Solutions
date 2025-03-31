08b88e252a09d4c2bfdb5f001aa6a853b68d522b
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-03-31 01:46:40 +0000

    [LeetCode Sync] Runtime - 121 ms (89.27%), Memory - 30.2 MB (68.38%)

--- Solutions/2681-put-marbles-in-bags/solution.py
+++ Solutions/2681-put-marbles-in-bags/solution.py
@@ -1,17 +1,11 @@
-#Editorial Solution
 class Solution:
-    def putMarbles(self, weights: List[int], k: int) -> int:   
-        # We collect and sort the value of all n - 1 pairs.
-        n = len(weights)
-        pair_weights = [0] * (n - 1)
-        for i in range(n - 1):
-            pair_weights[i] = weights[i] + weights[i + 1]
-        pair_weights.sort()
-        
-        # Get the difference between the largest k - 1 values and the 
-        # smallest k - 1 values.
-        answer = 0
-        for i in range(k - 1):
-            answer += pair_weights[n - 2 - i] - pair_weights[i]
-            
-        return answer
+    def putMarbles(self, weights: List[int], k: int) -> int:
+        if len(weights) == k or k == 1:
+            return 0
+
+        # Compute the sum of adjacent pairs
+        wt_sum = [weights[i] + weights[i + 1] for i in range(len(weights) - 1)]
+        wt_sum.sort()
+
+        # Calculate the difference between the maximum and minimum sums
+        return sum(wt_sum[-(k - 1):]) - sum(wt_sum[:k - 1])
