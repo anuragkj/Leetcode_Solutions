a134544f347145379cea1bcc7ce9714bd80ed0cd
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-05-11 00:46:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18 MB (18.18%)

--- Solutions/1293-three-consecutive-odds/solution.py
+++ Solutions/1293-three-consecutive-odds/solution.py
@@ -1,11 +1,9 @@
 class Solution:
-    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
-        count = 0
-        for i in arr:
-            if i % 2 == 0:
-                count = 0
-            else:
-                count += 1
-                if count == 3:
-                    return True
+    def threeConsecutiveOdds(self, arr: list[int]) -> bool:
+        # Loop through the array up to the third-to-last element
+        for i in range(len(arr) - 2):
+            product = arr[i] * arr[i + 1] * arr[i + 2]
+            # Check if the product is odd
+            if product % 2 == 1:
+                return True
         return False
