c1e86d3c6f38364493a1e314226a3f8e4f02fbec
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-05-23 08:40:22 +0000

    [LeetCode Sync] Runtime - 59 ms (40.70%), Memory - 26.2 MB (47.40%)

--- Solutions/3307-find-the-maximum-sum-of-node-values/solution.py
+++ Solutions/3307-find-the-maximum-sum-of-node-values/solution.py
@@ -1,22 +1,15 @@
 class Solution:
-    def maxSumOfNodes(self, index, isEven, nums, k, memo):
-        if index == len(nums):
-            # If the operation is performed on an odd number of elements return INT_MIN
-            return 0 if isEven == 1 else -float("inf")
-        if memo[index][isEven] != -1:
-            return memo[index][isEven]
+    def maximumValueSum(self, nums, k, edges):
+        base_sum = sum(nums)
+        gains = [(num ^ k) - num for num in nums]
+        gains.sort(reverse=True)
 
-        # No operation performed on the element
-        noXorDone = nums[index] + self.maxSumOfNodes(index + 1, isEven, nums, k, memo)
-        # XOR operation is performed on the element
-        xorDone = (nums[index] ^ k) + self.maxSumOfNodes(
-            index + 1, isEven ^ 1, nums, k, memo
-        )
+        max_gain = 0
+        current_gain = 0
 
-        # Memoize and return the result
-        memo[index][isEven] = max(xorDone, noXorDone)
-        return memo[index][isEven]
+        for i, g in enumerate(gains):
+            current_gain += g
+            if (i + 1) % 2 == 0:
+                max_gain = max(max_gain, current_gain)
 
-    def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
-        memo = [[-1] * 2 for _ in range(len(nums))]
-        return self.maxSumOfNodes(0, 1, nums, k, memo)
+        return base_sum + max_gain
