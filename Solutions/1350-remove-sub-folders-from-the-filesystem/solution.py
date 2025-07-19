78f9d3e5734d346996d8ba524d370beedcf3f679
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-07-19 05:21:36 +0000

    [LeetCode Sync] Runtime - 51 ms (40.94%), Memory - 30.6 MB (63.19%)

--- Solutions/1350-remove-sub-folders-from-the-filesystem/solution.py
+++ Solutions/1350-remove-sub-folders-from-the-filesystem/solution.py
@@ -1,25 +1,29 @@
-class TrieNode():
-    def __init__(self):
-        self.is_word= False
-        self.children={}
-
 class Solution:
-    def removeSubfolders(self, folder: List[str]) -> List[str]:
-        node = TrieNode()
-        folder.sort()
-        output=[]
-        for i in folder:
-            fol=i.split("/")
-            root=node
-            flag=0
-            for x in fol:
-                if x not in root.children:
-                    root.children[x] = TrieNode()
-                root= root.children[x]
-                if root.is_word:
-                    flag=1
+    def removeSubfolders(self, folder) -> list[str]:
+        # Create a set to store all folder paths for fast lookup
+        folder_set = set(folder)
+        result = []
+
+        # Iterate through each folder to check if it's a sub-folder
+        for f in folder:
+            is_sub_folder = False
+            prefix = f
+
+            # Check all prefixes of the current folder path
+            while not prefix == "":
+                pos = prefix.rfind("/")
+                if pos == -1:
                     break
-            if flag==0:
-                root.is_word= True
-                output.append(i)
-        return output                    
+
+                # Reduce the prefix to its parent folder
+                prefix = prefix[0:pos]
+
+                # If the parent folder exists in the set, mark as sub-folder
+                if prefix in folder_set:
+                    is_sub_folder = True
+                    break
+
+            # If not a sub-folder, add it to the result
+            if not is_sub_folder:
+                result.append(f)
+        return result
