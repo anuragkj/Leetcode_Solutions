a67ccfc7f3434f234f96d030d93dd5a95bba4539
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-08-03 06:15:06 +0000

    [LeetCode Sync] Runtime - 5564 ms (17.65%), Memory - 18.1 MB (21.05%)

--- Solutions/0079-word-search/solution.py
+++ Solutions/0079-word-search/solution.py
@@ -1,22 +1,33 @@
 class Solution:
-    def exist(self, board, word):
-        def backtrack(i, j, k):
-            if k == len(word):
-                return True
-            if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j] != word[k]:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        rows, cols = len(board), len(board[0])
+
+        def dfs(i, j, word_so_far, visited):
+            if (i, j) in visited:
                 return False
-            
-            temp = board[i][j]
-            board[i][j] = ''
-            
-            if backtrack(i+1, j, k+1) or backtrack(i-1, j, k+1) or backtrack(i, j+1, k+1) or backtrack(i, j-1, k+1):
+            if not (0 <= i < rows and 0 <= j < cols):
+                return False
+            if board[i][j] != word[len(word_so_far)]:
+                return False
+
+            word_so_far += board[i][j]
+            if word_so_far == word:
                 return True
-            
-            board[i][j] = temp
-            return False
-        
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if backtrack(i, j, 0):
+
+            visited.add((i, j))
+            dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+            for dir_x, dir_y in dirs:
+                new_i, new_j = i + dir_x, j + dir_y
+                if dfs(new_i, new_j, word_so_far, visited):
                     return True
+            visited.remove((i, j))  # backtrack
+
+            return False
+
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == word[0]:  # start DFS only from matching first char
+                    if dfs(i, j, "", set()):
+                        return True
         return False
+
