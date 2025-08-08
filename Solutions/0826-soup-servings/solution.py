3b3115e73f49978a76be2c745dcc73cdbe9916c4
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-08-08 18:48:37 +0000

    [LeetCode Sync] Runtime - 1782 ms (5.01%), Memory - 480.9 MB (5.27%)

--- Solutions/0826-soup-servings/solution.py
+++ Solutions/0826-soup-servings/solution.py
@@ -1,20 +1,12 @@
 class Solution:
     def soupServings(self, n: int) -> float:
-        if n > 4800: 
-            return 1.0
-        n = (n + 24) // 25
-        memo = dict()
+        if n>50000: return 1.0
+        @cache
+        def dfs(A, B):
+            if A<=0 and B>0: return 1
+            if A<=0 and B<=0: return 0.5
+            if A>0 and B<=0: return 0
+            return 0.25*(dfs(A-4, B)+dfs(A-3, B-1)+dfs(A-2, B-2)+dfs(A-1, B-3))
+        N=(n+24)//25
+        return dfs(N, N)
         
-        def dp(i, j):
-            if (i, j) in memo:
-                return memo[(i, j)]
-            if i <= 0 and j <= 0: 
-                return 0.5
-            if i <= 0: 
-                return 1.0
-            if j <= 0: 
-                return 0.0
-            memo[(i, j)] = 0.25 * (dp(i - 4, j) + dp(i - 3, j - 1) + dp(i - 2, j - 2) + dp(i - 1, j - 3))
-            return memo[(i, j)]
-        
-        return dp(n, n)
