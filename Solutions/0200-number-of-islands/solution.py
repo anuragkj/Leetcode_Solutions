333b5a50ec0259a7c82baf4d7e1535d2e35f3758
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-08-13 20:48:18 +0000

    [LeetCode Sync] Runtime - 270 ms (30.57%), Memory - 20.6 MB (37.80%)

--- Solutions/0200-number-of-islands/solution.py
+++ Solutions/0200-number-of-islands/solution.py
@@ -1,22 +1,30 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-        
+        ROWS = len(grid)
+        COLS = len(grid[0])
+
         def dfs(i, j):
-            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != '1':
-                return
-            grid[i][j] = '0'  # mark as visited
-            dfs(i+1, j)
-            dfs(i-1, j)
-            dfs(i, j+1)
-            dfs(i, j-1)
+            print(i,j)
+            grid[i][j] = "0"
+            dirs = [[0,1], [0,-1], [1,0],[-1,0]]
+
+            for x,y in dirs:
+                new_i = i+x
+                new_j = j+y
+                if new_i >= 0 and new_i < ROWS and new_j >=0 and new_j < COLS and grid[new_i][new_j] == "1":
+                    dfs(new_i, new_j)
+
+
+
         
-        num_islands = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    num_islands += 1
-                    dfs(i, j)
+        res = 0
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == "1":
+                    dfs(i,j)
+                    res+=1
+        return res
+
+
         
-        return num_islands
