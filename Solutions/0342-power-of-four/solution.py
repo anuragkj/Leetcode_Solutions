2e40e23e7a854982cd1b37a5e8af6ae8a8283828
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-08-15 06:15:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.8 MB (63.16%)

--- Solutions/0342-power-of-four/solution.py
+++ Solutions/0342-power-of-four/solution.py
@@ -1,4 +1,3 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        mask = 0x55555555
-        return n > 0 and (n & (n - 1)) == 0 and (n & mask) == n
+        return n>0 and n.bit_count()==1 and (n-1)%3==0
