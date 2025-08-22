c596cae99ae183f8096fb80655a26e757055ec90
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-08-22 03:44:35 +0000

    [LeetCode Sync] Runtime - 4 ms (3.19%), Memory - 18 MB (12.22%)

--- Solutions/0198-house-robber/solution.py
+++ Solutions/0198-house-robber/solution.py
@@ -1,9 +1,16 @@
-class Solution(object):
-    def rob(self, nums):
-        rob, norob = 0, 0
-        for num in nums:
-            newRob = norob + num
-            newNoRob = max(norob, rob)
-            rob, norob = newRob, newNoRob
-        return max(rob, norob)
-        
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def solve(nums, i, n):
+            nonlocal dp
+            if i >= n:
+                return 0
+            if dp[i] != -1:
+                return dp[i]
+            steal = nums[i] + solve(nums, i + 2, n)
+            skip = solve(nums, i + 1, n)
+            dp[i] = max(steal,skip)
+            return dp[i]
+
+        n = len(nums)
+        dp = [-1] * 101
+        return solve(nums,0,n)
