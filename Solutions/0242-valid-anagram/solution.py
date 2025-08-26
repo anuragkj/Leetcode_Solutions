52e88d11cf6cfc089513fde106343ab532277ee8
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-08-26 19:01:58 +0000

    [LeetCode Sync] Runtime - 11 ms (73.28%), Memory - 17.7 MB (85.80%)

--- Solutions/0242-valid-anagram/solution.py
+++ Solutions/0242-valid-anagram/solution.py
@@ -1,16 +1,9 @@
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:  
-        if len(s) != len(t):
-            return False
-
-        count = [0] * 26
-
-        for char in s:
-            count[ord(char) - ord('a')] += 1
-
-        for char in t:
-            if count[ord(char) - ord('a')] == 0:
-                return False
-            count[ord(char) - ord('a')] -= 1
-
-        return True
+    def isAnagram(self, s: str, t: str) -> bool:
+        sdict = {}
+        tdict = {}
+        for i in s:
+            sdict[i] = sdict.get(i, 0) + 1
+        for i in t:
+            tdict[i] = tdict.get(i, 0) + 1
+        return sdict == tdict
