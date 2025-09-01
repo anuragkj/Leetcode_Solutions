4be4e2694940a5abb247daf90a01535f579301b5
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-09-01 18:56:13 +0000

    [LeetCode Sync] Runtime - 1155 ms (52.71%), Memory - 54.8 MB (68.54%)

--- Solutions/1917-maximum-average-pass-ratio/solution.py
+++ Solutions/1917-maximum-average-pass-ratio/solution.py
@@ -1,13 +1,34 @@
 class Solution:
-    def maxAverageRatio(self, cls: List[List[int]], extra: int) -> float:
-        cls = [(p/t - (p+1)/(t+1), p, t) for p, t in cls]
-        heapify(cls)
+    def maxAverageRatio(
+        self, classes: List[List[int]], extraStudents: int
+    ) -> float:
+        # Lambda to calculate the gain of adding an extra student
+        def _calculate_gain(passes, total_students):
+            return (passes + 1) / (total_students + 1) - passes / total_students
 
-        if cls[0][0] == 0:
-            return 1
+        # Max heap to store (-gain, passes, total_students)
+        max_heap = []
+        for passes, total_students in classes:
+            gain = _calculate_gain(passes, total_students)
+            max_heap.append((-gain, passes, total_students))
 
-        for _ in range(extra):
-            _, p, t = heappop(cls)
-            heappush(cls, ((p+1)/(t+1) - (p+2)/(t+2), p+1, t+1))
+        # Use heapify to transform the list into a valid heap in O(n)
+        heapq.heapify(max_heap)
 
-        return sum(p/t for _, p, t in cls) / len(cls)
+        # Distribute extra students
+        for _ in range(extraStudents):
+            current_gain, passes, total_students = heapq.heappop(max_heap)
+            heapq.heappush(
+                max_heap,
+                (
+                    -_calculate_gain(passes + 1, total_students + 1),
+                    passes + 1,
+                    total_students + 1,
+                ),
+            )
+
+        # Calculate the final average pass ratio
+        total_pass_ratio = sum(
+            passes / total_students for _, passes, total_students in max_heap
+        )
+        return total_pass_ratio / len(classes)
