9ffc09de5182da746abd479c76b85668827fc0f2
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-09-23 02:30:11 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (88.04%)

--- Solutions/0165-compare-version-numbers/solution.py
+++ Solutions/0165-compare-version-numbers/solution.py
@@ -1,22 +1,24 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        def helper(s: str, idx: int) -> List[int]:
-            num = 0
-            while idx < len(s):
-                if s[idx] == '.':
-                    break
-                else:
-                    num = num * 10 + int(s[idx])
-                idx += 1
-            return [num, idx+1]
+        index1, index2 = 0, 0
+        length1, length2 = len(version1), len(version2)
 
-        i = j = 0
-        while(i < len(version1) or j < len(version2)):
-            v1, i = helper(version1, i)
-            v2, j = helper(version2, j)
-            if v1 > v2:
+        while index1 < length1 or index2 < length2:
+            number1 = 0
+            while index1 < length1 and version1[index1] != '.':
+                number1 = number1 * 10 + int(version1[index1])
+                index1 += 1
+            index1 += 1  
+
+            number2 = 0
+            while index2 < length2 and version2[index2] != '.':
+                number2 = number2 * 10 + int(version2[index2])
+                index2 += 1
+            index2 += 1  
+
+            if number1 > number2:
                 return 1
-            elif v1 < v2:
+            elif number1 < number2:
                 return -1
 
         return 0
