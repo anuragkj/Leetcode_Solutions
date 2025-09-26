79fa08fa45ca4b8ec9f0671f16dad958a7744fcb
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-09-26 06:24:52 +0000

    [LeetCode Sync] Runtime - 481 ms (29.45%), Memory - 18 MB (11.79%)

---
+++ Solutions/0611-valid-triangle-number/solution.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def triangleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        ans = 0
+        L = len(nums)
+        # c < (a+b)
+        for i in range(L-1,1,-1):
+            c = nums[i]
+            start = 0
+            end = i-1
+            while start < end:
+                if nums[start] + nums[end] > c:
+                    ans += end - start
+                    end -= 1
+                elif nums[start] + nums[end] <= c:
+                    start += 1
+        return ans
+                
+        
+        
