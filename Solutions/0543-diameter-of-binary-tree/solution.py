4964b308271bcb7dbe40c9dbc29f2a2b28a52b3b
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-10-02 06:24:24 +0000

    [LeetCode Sync] Runtime - 3 ms (94.77%), Memory - 20.7 MB (83.60%)

--- Solutions/0543-diameter-of-binary-tree/solution.py
+++ Solutions/0543-diameter-of-binary-tree/solution.py
@@ -1,24 +1,18 @@
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        # Define a recursive function to calculate the diameter
-        def diameter(node, res):
-            # Base case: if the node is None, return 0
-            if not node:
+        res = 0
+
+        def dfs(root):
+            if not root:
                 return 0
             
-            # Recursively calculate the diameter of left and right subtrees
-            left = diameter(node.left, res)
-            right = diameter(node.right, res)
+            l = dfs(root.left)
+            r = dfs(root.right)
 
-            # Update the maximum diameter encountered so far
-            res[0] = max(res[0], left + right)
-            
-            # Return the depth of the current node
-            return max(left, right) + 1
-        
-        # Initialize a list to hold the maximum diameter encountered
-        res = [0]
-        # Call the diameter function starting from the root
-        diameter(root, res)
-        # Return the maximum diameter encountered
-        return res[0]
+            nonlocal res
+            res = max(res, l + r)
+
+            return 1 + max(l, r)
+
+        dfs(root)
+        return res
