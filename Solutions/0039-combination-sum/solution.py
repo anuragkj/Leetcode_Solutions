4891c4105b6a3e51c15519bf8b1d1da96b00aa62
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-10-18 03:40:15 +0000

    [LeetCode Sync] Runtime - 12 ms (37.82%), Memory - 18 MB (39.17%)

--- Solutions/0039-combination-sum/solution.py
+++ Solutions/0039-combination-sum/solution.py
@@ -1,17 +1,20 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-        def backtrack(start, path, total):
+        
+        def dfs(ind, curr, total):
             if total == target:
-                res.append(path[:])
+                res.append(curr.copy())
                 return
-            if total > target:
+            if total > target or ind >= len(candidates):
                 return
 
-            for i in range(start, len(candidates)):
-                path.append(candidates[i])
-                backtrack(i, path, total + candidates[i]) 
-                path.pop()
+            curr.append(candidates[ind])
+            dfs(ind, curr, total+candidates[ind])
+            curr.pop()
+            dfs(ind+1, curr, total)
 
-        backtrack(0, [], 0)
+            return
+        dfs(0,[],0)
         return res
+
