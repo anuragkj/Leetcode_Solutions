55ef3ec7bfb9ba5c26016e1e6bc6288063191f0b
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-10-18 07:58:48 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18 MB (20.68%)

--- Solutions/0078-subsets/solution.py
+++ Solutions/0078-subsets/solution.py
@@ -1,13 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        output = []
+        res = []
+        visisted = set()
+        def dfs(i, path):
+            if i >= len(nums):
+                return
+            res.append(path.copy())
+            for j in range(i+1, len(nums)):
+                path.append(nums[j])
+                dfs(j, path)
+                path.pop()
+            return
+        dfs(-1, [])
+        return res
+            
 
-        for i in range(2**n, 2 ** (n + 1)):
-            # generate bitmask, from 0..00 to 1..11
-            bitmask = bin(i)[3:]
-
-            # append subset corresponding to that bitmask
-            output.append([nums[j] for j in range(n) if bitmask[j] == "1"])
-
-        return output
