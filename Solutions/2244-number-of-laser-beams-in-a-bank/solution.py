4a1ad0f73b06b318a6d7cff4d2b458b607046646
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-10-27 02:27:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.5 MB (46.99%)

--- Solutions/2244-number-of-laser-beams-in-a-bank/solution.py
+++ Solutions/2244-number-of-laser-beams-in-a-bank/solution.py
@@ -1,17 +1,13 @@
+from typing import List
+
 class Solution:
     def numberOfBeams(self, bank: List[str]) -> int:
-        ret = 0
-        last = -1
-
-        for i in bank:
-            curr = 0
-            for j in i:
-                curr += int(j)
-            if last == -1:
-                last = curr
-                continue
-            if curr != 0:
-                ret += curr * last
-                last = curr
-
-        return ret
+        ans = 0
+        prev = 0
+        
+        for row in bank:
+            cnt = row.count('1')
+            if cnt > 0:
+                ans += prev * cnt
+                prev = cnt
+        return ans
