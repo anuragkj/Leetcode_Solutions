2cf6bcbfc3115220a61f48210fd0fe485e111a78
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-11-29 18:51:09 +0000

    [LeetCode Sync] Runtime - 147 ms (30.59%), Memory - 41.1 MB (16.50%)

--- Solutions/3058-maximum-number-of-k-divisible-components/solution.py
+++ Solutions/3058-maximum-number-of-k-divisible-components/solution.py
@@ -1,23 +1,25 @@
-class Solution:  
-    def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:  
-        adj = defaultdict(list)  
-  
-        for n1, n2 in edges:  
-            adj[n1].append(n2)  
-            adj[n2].append(n1)  
-  
-        res = 0  
-        def dfs(cur, parent):  
-            total = values[cur]  
-  
-            for child in adj[cur]:  
-                if child != parent:  
-                    total += dfs(child, cur)  
-  
-            if total % k == 0:  
-                nonlocal res  
-                res += 1  
-            return total  
-  
-        dfs(0, -1)  
-        return res  
+class Solution:
+    def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
+        self.components = 0
+
+        adj_list = defaultdict(list)
+        for i,j in edges:
+            adj_list[i].append(j)
+            adj_list[j].append(i)
+        def dfs(root, parent):
+            
+            if root is None:
+                return 0
+            total_sum = 0
+            for nei in adj_list[root]:
+                if nei!=parent:
+                    total_sum += dfs(nei, root)
+            total_sum += values[root]
+            if total_sum%k == 0:
+                self.components+=1
+            
+            return total_sum
+
+        dfs(0,-1)
+        return self.components
+        
