e8ed658c5fc0c4de692dacdb95107d028b454890
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-12-03 01:54:43 +0000

    [LeetCode Sync] Runtime - 45 ms (74.89%), Memory - 18.7 MB (72.31%)

--- Solutions/0729-my-calendar-i/solution.py
+++ Solutions/0729-my-calendar-i/solution.py
@@ -1,12 +1,24 @@
-from sortedcontainers import SortedList
-
+from bisect import bisect_left, bisect_right
 class MyCalendar:
-    def __init__(self):
-        self.calendar = SortedList()
 
-    def book(self, start: int, end: int) -> bool:
-        idx = self.calendar.bisect_right((start, end))
-        if (idx > 0 and self.calendar[idx-1][1] > start) or (idx < len(self.calendar) and self.calendar[idx][0] < end):
-            return False
-        self.calendar.add((start, end))
-        return True
+    def __init__(self):
+        self.bookings = []
+        self.len_book = 0
+        
+
+    def book(self, startTime: int, endTime: int) -> bool:
+        left_add = bisect_left(self.bookings, startTime, key = lambda x: x[0])
+        right_add = bisect_right(self.bookings, endTime, key = lambda x: x[1])
+        if left_add == right_add:
+            if (left_add == 0 or self.bookings[left_add-1][1] <= startTime) and (right_add == self.len_book or self.bookings[right_add][0] >= endTime):
+                self.bookings.insert(left_add, [startTime, endTime])
+                self.len_book += 1
+                return True
+        return False
+        
+        
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(startTime,endTime)
