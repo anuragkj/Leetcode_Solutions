9962bb9509ee69ca4b58147af6b84da4926bb2eb
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-12-15 19:50:12 +0000

    [LeetCode Sync] Runtime - 1 ms (64.39%), Memory - 19.7 MB (59.41%)

--- Solutions/0057-insert-interval/solution.py
+++ Solutions/0057-insert-interval/solution.py
@@ -1,33 +1,14 @@
 class Solution:
-    def insert(
-        self, intervals: List[List[int]], newInterval: List[int]
-    ) -> List[List[int]]:
-        # If the intervals vector is empty, return a vector containing the newInterval
-        if not intervals:
-            return [newInterval]
-
-        n = len(intervals)
-        target = newInterval[0]
-        left, right = 0, n - 1
-
-        # Binary search to find the position to insert newInterval
-        while left <= right:
-            mid = (left + right) // 2
-            if intervals[mid][0] < target:
-                left = mid + 1
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        bisect.insort(intervals, newInterval)
+        ret = []
+        to_add = intervals[0]
+        for i in range(1, len(intervals)):
+            new = intervals[i]
+            if new[0] > to_add[1]:
+                ret.append(to_add)
+                to_add = new
             else:
-                right = mid - 1
-
-        # Insert newInterval at the found position
-        intervals.insert(left, newInterval)
-
-        # Merge overlapping intervals
-        res = []
-        for interval in intervals:
-            # If res is empty or there is no overlap, add the interval to the result
-            if not res or res[-1][1] < interval[0]:
-                res.append(interval)
-            # If there is an overlap, merge the intervals by updating the end of the last interval in res
-            else:
-                res[-1][1] = max(res[-1][1], interval[1])
-        return res
+                to_add = [min(to_add[0],new[0]), max(to_add[1],new[1])]
+        ret.append(to_add)
+        return ret
