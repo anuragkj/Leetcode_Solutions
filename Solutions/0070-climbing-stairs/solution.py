96f9bb77a5ab7b73408ff8860c21802ad1ecabbb
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-12-19 19:44:00 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.3 MB (99.83%)

--- Solutions/0070-climbing-stairs/solution.py
+++ Solutions/0070-climbing-stairs/solution.py
@@ -2,13 +2,11 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         @cache
         def dfs(i):
-            if i == 0:
+            if i == n:
                 return 1
+            if i > n:
+                return 0
             
-            one_stair = dfs(i-1)
-            two_stair = 0
-            if i >=2:
-                two_stair = dfs(i-2)
-            return one_stair + two_stair
-        return dfs(n)
-        
+            return dfs(i+1) + dfs(i+2)
+
+        return dfs(0)
