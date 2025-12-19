ebd18a73beaf51a5abfb57c53ea5a11673e830f6
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-12-19 21:06:55 +0000

    [LeetCode Sync] Runtime - 7 ms (3.46%), Memory - 18 MB (29.54%)

--- Solutions/0091-decode-ways/solution.py
+++ Solutions/0091-decode-ways/solution.py
@@ -1,34 +1,19 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        # memo[i] will store the number of ways to decode s[i:]
-        memo = {} 
-
-        def dfs(i):
-            # If we've already computed this subproblem, return the stored result
-            if i in memo:
-                return memo[i]
-
-            # Base Case 1: If we've reached the end of the string, we've found one valid way.
+        @cache
+        def dfs(i,pick):
             if i == len(s):
-                return 1
-
-            # Base Case 2: A code cannot start with '0'. This is an invalid path.
-            if s[i] == "0":
                 return 0
-
-            # --- Recursive Step ---
-
-            # Option 1: Decode the current single digit (s[i])
-            # The number of ways for this option is the number of ways to decode the rest of the string.
-            res = dfs(i + 1)
-
-            # Option 2: Decode the current two digits (s[i:i+2])
-            # This is only possible if we have at least 2 digits left AND the number is <= 26.
-            if i + 1 < len(s) and int(s[i:i+2]) <= 26:
-                res += dfs(i + 2)
-            
-            # Store the result for this subproblem before returning
-            memo[i] = res
-            return res
+            if i == len(s) - 1 and int(pick)*10 + int(s[i]) in range(1,27):
+                return 1
+            total = 0
+            #commit/carry
+            if int(pick)*10 + int(s[i]) in range(1,27):
+                print(int(pick)*10 + int(s[i]))
+                total += dfs(i+1, 0)
+            if int(pick)==0 and int(s[i])!=0:
+                total += dfs(i+1, s[i])
+            return total
         
-        return dfs(0)
+        return dfs(0,0)
+
