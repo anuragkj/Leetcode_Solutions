e8bcca46bdf80ff01089171e02405b6a41600884
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-12-19 20:38:38 +0000

    [LeetCode Sync] Runtime - 129 ms (60.64%), Memory - 17.1 MB (100.00%)

--- Solutions/0647-palindromic-substrings/solution.py
+++ Solutions/0647-palindromic-substrings/solution.py
@@ -1,15 +1,21 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        def countpali(left, right):
-            if left < 0 or right >= len(s):
-                return 0
-            if s[left] == s[right]:
-                return 1 + countpali(left - 1, right + 1)
-            return 0
+        def countpalindrome(i,j):
+            count = 0
+            while(i>=0 and j<len(s)):
+                if s[i] == s[j]:
+                    count+=1
+                    i-=1
+                    j+=1
+                else:
+                    break
+            
+            return count
 
-        ret = 0
-        for i in range(len(s)):
-            countodd = countpali(i-1, i+1)
-            counteven = countpali(i, i+1)
-            ret += countodd + counteven
-        return len(s)+ret
+        total = 0        
+        for i in range(len(s)-1):
+            total+=countpalindrome(i,i)
+            total+=countpalindrome(i,i+1)
+        total+=1
+        return total
+
