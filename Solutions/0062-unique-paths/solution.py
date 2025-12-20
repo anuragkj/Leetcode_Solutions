e975cd76ef820ae5acac5896a41a3606932508b1
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-12-20 04:10:38 +0000

    [LeetCode Sync] Runtime - 4 ms (8.49%), Memory - 17.9 MB (25.05%)

--- Solutions/0062-unique-paths/solution.py
+++ Solutions/0062-unique-paths/solution.py
@@ -1,19 +1,18 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        memo = {}
-        def dfs(x,y):
-            if x == m or y == n:
-                return 0
-            if x==m-1 and y == n-1:
+        @cache
+        def dfs(i,j):
+            if i==m-1 and j==n-1:
                 return 1
-            if (x,y) in memo:
-                return memo[(x,y)]
-            ways = 0
-            ways+= dfs(x+1, y)
-            ways += dfs(x, y+1)
-
-            memo[(x,y)] = ways
-
-            return ways
+            if i < 0 or i >= m or j <0 or j >= n:
+                return 0
+            dirs = [(1,0),(0,1)]
+            val = 0
+            for dx,dy in dirs:
+                new_x = i+dx
+                new_y = j+dy
+                if new_x >= 0 and new_x < m and new_y >= 0 and new_y < n:
+                    val+=dfs(new_x,new_y)
+            return val
         return dfs(0,0)
         
