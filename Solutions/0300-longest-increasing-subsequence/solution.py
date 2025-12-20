731a39816c8ba0e5a054e97a58a900ddc37778f3
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-12-20 03:30:32 +0000

    [LeetCode Sync] Runtime - 3 ms (95.72%), Memory - 17.7 MB (98.69%)

--- Solutions/0300-longest-increasing-subsequence/solution.py
+++ Solutions/0300-longest-increasing-subsequence/solution.py
@@ -1,14 +1,10 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        n=len(nums)
-        ahead=[0]*(n+1)
-        curr=[0]*(n+1)
-        for ind in range(n-1,-1,-1):
-            for prev in range(ind-1,-2,-1):
-                not_take=0+ahead[prev+1]
-                take=0
-                if prev==-1 or nums[ind]>nums[prev]:
-                    take=1+ahead[ind+1]
-                curr[prev+1]=max(take,not_take)
-            ahead=curr[:]
-        return curr[0]
+        res = []
+        for i in nums:
+            if len(res) == 0 or res[-1] < i:
+                res.append(i)
+            else:
+                pos = bisect.bisect_left(res,i)
+                res[pos] = i
+        return len(res)
