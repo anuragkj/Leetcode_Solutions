402f16c736d04f18436c18bc0c8d3d016c5012b6
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-12-20 19:29:09 +0000

    [LeetCode Sync] Runtime - 7 ms (82.03%), Memory - 17.5 MB (99.03%)

--- Solutions/0039-combination-sum/solution.py
+++ Solutions/0039-combination-sum/solution.py
@@ -1,20 +1,22 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
-        
-        def dfs(ind, curr, total):
-            if total == target:
-                res.append(curr.copy())
-                return
-            if total > target or ind >= len(candidates):
-                return
+        candidates.sort()
+        ret = []
 
-            curr.append(candidates[ind])
-            dfs(ind, curr, total+candidates[ind])
-            curr.pop()
-            dfs(ind+1, curr, total)
-
+        def dfs(i,tsum,path):
+            nonlocal ret
+            if tsum == target:
+                ret.append(path.copy())
+            if tsum > target:
+                return
+            
+            for j in range(i, len(candidates)):
+                if tsum + candidates[j] <= target:
+                    path.append(candidates[j])
+                    dfs(j,tsum+candidates[j],path)
+                    path.pop()
             return
-        dfs(0,[],0)
-        return res
+        dfs(0,0,[])
+        return ret
+            
 
