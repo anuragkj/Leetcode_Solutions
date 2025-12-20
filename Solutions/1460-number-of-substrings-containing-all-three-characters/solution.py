bbcf158e624ebb501f3e5bbaf0a0872e9c04c9bf
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-12-20 07:51:43 +0000

    [LeetCode Sync] Runtime - 139 ms (23.02%), Memory - 17.7 MB (98.55%)

--- Solutions/1460-number-of-substrings-containing-all-three-characters/solution.py
+++ Solutions/1460-number-of-substrings-containing-all-three-characters/solution.py
@@ -1,13 +1,14 @@
-class Solution(object):
-    def numberOfSubstrings(self, s):
-        count = [0] * 3  
-        left = 0  
-        result = 0  
-        for i in range(len(s)):
-            count[ord(s[i]) - ord('a')] += 1  
-
-            while count[0] > 0 and count[1] > 0 and count[2] > 0:
-                result += len(s) - i  
-                count[ord(s[left]) - ord('a')] -= 1  
-                left += 1 
-        return result
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        dic = defaultdict(int)
+        l = 0
+        ret = 0
+        for r in range(len(s)):
+            dic[s[r]] += 1
+            while len(dic.keys()) == 3:
+                ret += len(s) - r
+                dic[s[l]] -= 1
+                if dic[s[l]]==0:
+                    del dic[s[l]]
+                l+=1
+        return ret
