da48797d93315274c7852eb859124f7ad49303f0
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-12-21 05:23:33 +0000

    [LeetCode Sync] Runtime - 23 ms (93.47%), Memory - 18.7 MB (99.68%)

--- Solutions/0417-pacific-atlantic-water-flow/solution.py
+++ Solutions/0417-pacific-atlantic-water-flow/solution.py
@@ -1,21 +1,33 @@
 class Solution:
-    def pacificAtlantic(self, heights):
-        if not heights:
-            return []
-        m, n = len(heights), len(heights[0])
-        directions = [(1,0), (-1,0), (0,1), (0,-1)]
-        
-        def dfs(i, j, visited):
-            visited.add((i, j))
-            for dx, dy in directions:
-                x, y = i + dx, j + dy
-                if 0 <= x < m and 0 <= y < n:
-                    if (x, y) not in visited and heights[x][y] >= heights[i][j]:
-                        dfs(x, y, visited)
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        visitp = set()
+        visita = set()
+        rows = len(heights)
+        cols = len(heights[0])
+        dirs = ((1,0),(-1,0),(0,1),(0,-1))
 
-        pacific, atlantic = set(), set()
-        for j in range(n): dfs(0, j, pacific)
-        for i in range(m): dfs(i, 0, pacific)
-        for j in range(n): dfs(m-1, j, atlantic)
-        for i in range(m): dfs(i, n-1, atlantic)
-        return list(pacific & atlantic)
+        def dfs(x,y,visited):
+            if x<0 or x>=rows or y<0 or y>=cols or (x,y) in visited:
+                return
+            visited.add((x,y))
+            for dx,dy in dirs:
+                nx, ny = x+dx, y+dy
+                if nx >=0 and nx<rows and ny>=0 and ny<cols and (nx,ny) not in visited and heights[nx][ny]>=heights[x][y]:
+                    dfs(nx,ny,visited)
+        
+        for r in range(rows):
+            dfs(r,0,visitp)
+            dfs(r,cols-1,visita)
+
+
+        for c in range(cols):
+            dfs(0,c,visitp)
+            dfs(rows-1,c,visita)
+
+        ret = []
+        for i in visita:
+            if i in visitp:
+                ret.append(i)
+        
+
+        return(ret)
