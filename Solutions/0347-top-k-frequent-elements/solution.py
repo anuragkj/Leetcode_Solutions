9021f528e46a604346ec623a442f4788550fbcb7
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-12-21 07:40:28 +0000

    [LeetCode Sync] Runtime - 7 ms (51.43%), Memory - 21 MB (88.35%)

--- Solutions/0347-top-k-frequent-elements/solution.py
+++ Solutions/0347-top-k-frequent-elements/solution.py
@@ -1,15 +1,33 @@
-from collections import Counter
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]: 
-        # O(1) time 
-        if k == len(nums):
-            return nums
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        def partition(l,r,arr):
+            pivot = arr[r][0]
+            i = l-1
+            for j in range(l,r):
+                if arr[j][0] <=pivot:
+                    i+=1
+                    arr[i],arr[j] = arr[j],arr[i]
+            arr[i+1],arr[r] = arr[r],arr[i+1]
+            return i+1
+        freq = Counter(nums)
+        freq_arr = []
+        for ki in freq:
+            freq_arr.append([freq[ki],ki])
+        l=0
+        r=len(freq_arr)-1
+        target = len(freq_arr) - k
+        while(l<r):
+            pivot = partition(l,r,freq_arr)
+            if pivot==target:
+                break
+            if pivot>target:
+                r = pivot-1
+            else:
+                l = pivot +1
         
-        dic = Counter(nums)
-        lst = list((-dic[k],k) for k in dic.keys())
-        heapq.heapify(lst)
         ret = []
-        for i in range(k):
-            ele = heapq.heappop(lst)
-            ret.append(ele[1])
+        print(freq_arr)
+        for p,q in freq_arr[target:]:
+            ret.append(q)
         return ret
+
