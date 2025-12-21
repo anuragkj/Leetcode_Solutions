9fc7ac6e47114de5d977d813b88cc7ba020c692c
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-12-21 04:23:41 +0000

    [LeetCode Sync] Runtime - 264 ms (33.24%), Memory - 24.9 MB (32.28%)

--- Solutions/0200-number-of-islands/solution.py
+++ Solutions/0200-number-of-islands/solution.py
@@ -1,30 +1,24 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        ROWS = len(grid)
-        COLS = len(grid[0])
+        rows = len(grid)
+        cols = len(grid[0])
+        dirs = ((1,0),(0,1),(-1,0),(0,-1))
+        visited = set()
 
-        def dfs(i, j):
-            print(i,j)
-            grid[i][j] = "0"
-            dirs = [[0,1], [0,-1], [1,0],[-1,0]]
-
-            for x,y in dirs:
-                new_i = i+x
-                new_j = j+y
-                if new_i >= 0 and new_i < ROWS and new_j >=0 and new_j < COLS and grid[new_i][new_j] == "1":
-                    dfs(new_i, new_j)
-
-
-
+        def dfs(x,y,visited):
+            if x<0 or x>=rows or y<0 or y>=cols or grid[x][y]!="1" or ((x,y) in visited):
+                return
+            visited.add((x,y))
+            for dx,dy in dirs:
+                nx, ny = x+dx, y+dy
+                dfs(nx,ny,visited)
         
-        res = 0
+        count = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r,c) not in visited:
+                    count+=1
+                    dfs(r,c,visited)
 
-        for i in range(ROWS):
-            for j in range(COLS):
-                if grid[i][j] == "1":
-                    dfs(i,j)
-                    res+=1
-        return res
+        return count
 
-
-        
