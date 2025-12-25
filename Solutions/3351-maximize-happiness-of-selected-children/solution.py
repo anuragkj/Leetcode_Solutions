0a78ebf81324202c94efb8752046c38f16389154
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-12-25 19:04:09 +0000

    [LeetCode Sync] Runtime - 786 ms (24.83%), Memory - 43.4 MB (99.83%)

--- Solutions/3351-maximize-happiness-of-selected-children/solution.py
+++ Solutions/3351-maximize-happiness-of-selected-children/solution.py
@@ -1,16 +1,10 @@
-class Solution:
-    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
-        max_heap = [-h for h in happiness]
-        heapq.heapify(max_heap)
-        
-        total_happiness_sum = 0
-        turns = 0
-
-        for i in range(k):
-            # Invert again to get the original value
-            total_happiness_sum += max(-heapq.heappop(max_heap) - turns, 0)
-
-            # Increment turns for the next iteration
-            turns += 1
-            
-        return total_happiness_sum
+class Solution(object):
+    def maximumHappinessSum(self, happiness, k):
+        happiness.sort()
+        res = 0
+        count = 0
+        for i in range(len(happiness) - 1, len(happiness) - k - 1, -1):
+            if happiness[i] - count > 0:
+                res += (happiness[i] - count)
+            count += 1
+        return res
