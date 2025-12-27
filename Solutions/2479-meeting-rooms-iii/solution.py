02b3b62f0500961806ae5558b5679e9d557a5e8c
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-12-27 02:27:40 +0000

    [LeetCode Sync] Runtime - 163 ms (95.66%), Memory - 51 MB (98.34%)

--- Solutions/2479-meeting-rooms-iii/solution.py
+++ Solutions/2479-meeting-rooms-iii/solution.py
@@ -1,30 +1,22 @@
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
-        meetings.sort(key = lambda x: x[0])
-        current_meets = [] #(end_time, room_num)
-        free_rooms = [i for i in range(n)]
-        heapq.heapify(free_rooms)
+        ready = [r for r in range(n)]
+        rooms = []
+        heapify(ready)
+        res = [0] * n
 
-        count = [0]*n
+        for s, e in sorted(meetings):
+            while rooms and rooms[0][0] <= s:
+                t, r = heappop(rooms)
+                heappush(ready, r)
 
-        for arrival, end in meetings:
-            while current_meets and current_meets[0][0] <= arrival:
-                _, freed_room = heapq.heappop(current_meets)
-                heapq.heappush(free_rooms, freed_room)
-
-            if free_rooms:
-                available_room = heapq.heappop(free_rooms)
-                heapq.heappush(current_meets,(end,available_room))
-                count[available_room] += 1
+            if ready:
+                r = heappop(ready)
+                heappush(rooms, [e, r])
             else:
-                first_free_time, first_avail_room = heapq.heappop(current_meets)
-                heapq.heappush(current_meets,(first_free_time + (end - arrival),first_avail_room))
-                count[first_avail_room] += 1
+                t, r = heappop(rooms)
+                heappush(rooms, [t + e - s, r])
 
-        max_count = max(count)
-        ret = count.index(max_count)
-        return ret
+            res[r] += 1
 
-
-
-        
+        return res.index(max(res))
