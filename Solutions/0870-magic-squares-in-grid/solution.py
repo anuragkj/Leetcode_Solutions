235fd1732dba5dd3f86f96c49a22b33f470c57c9
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-12-30 01:50:52 +0000

    [LeetCode Sync] Runtime - 3 ms (39.51%), Memory - 17.3 MB (100.00%)

--- Solutions/0870-magic-squares-in-grid/solution.py
+++ Solutions/0870-magic-squares-in-grid/solution.py
@@ -1,27 +1,5 @@
 class Solution:
-    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
-        if m < 3 or n < 3:
-            return 0 
-        count = 0
-        
-        for i in range(m - 2):
-            for j in range(n - 2):
-                d = set()
-                for k in range(i, i + 3):
-                    for l in range(j, j + 3):
-                        d.add(grid[k][l])
-                
-                if d == set(range(1, 10)):
-                    if (grid[i][j] + grid[i][j+1] + grid[i][j+2] == 
-                        grid[i+1][j] + grid[i+1][j+1] + grid[i+1][j+2] ==
-                        grid[i+2][j] + grid[i+2][j+1] + grid[i+2][j+2] ==
-                        grid[i][j] + grid[i+1][j] + grid[i+2][j] ==
-                        grid[i][j+1] + grid[i+1][j+1] + grid[i+2][j+1] ==
-                        grid[i][j+2] + grid[i+1][j+2] + grid[i+2][j+2] ==
-                        grid[i][j] + grid[i+1][j+1] + grid[i+2][j+2] ==
-                        grid[i][j+2] + grid[i+1][j+1] + grid[i+2][j]):
-                        count += 1
-                
-        return count
+    def numMagicSquaresInside(self, g: List[List[int]]) -> int:
+        return sum(''.join(map(str,g[i][j:j+3]+g[i+1][j:j+3]+g[i+2][j:j+3])) in 
+                {'276951438','294753618','438951276','492357816','618753294','672159834','816357492','834159672'} 
+                for i,j in product(range(len(g)-2), range(len(g[0])-2)))
