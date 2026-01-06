486815e2d7e60709abb91ec969eb332026797722
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2026-01-06 01:15:51 +0000

    [LeetCode Sync] Runtime - 12 ms (92.49%), Memory - 21.7 MB (99.94%)

--- Solutions/1116-maximum-level-sum-of-a-binary-tree/solution.py
+++ Solutions/1116-maximum-level-sum-of-a-binary-tree/solution.py
@@ -1,59 +1,24 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def maxLevelSum(self, root: Optional[TreeNode]) -> int:
-        k = 1
-        q1 = [root]
-        q2 = []
-        max_val = root.val
-        output = 1
-        level = 1
-        while(True):
-            if k==1:
-                for i in q1:
-                    if i is not None:
-                        q2.append(i.left)
-                        q2.append(i.right)
-                sum = 0
-                flag = 0
-                for i in q2:
-                    if i is not None:
-                        sum+=i.val
-                        flag = 1
-                if flag == 0:
-                    break
-                if sum>max_val:
-                    max_val = sum
-                    output = level + 1
-                q1.clear()
-                k = 0
-            else:
-                for i in q2:
-                    if i is not None:
-                        q1.append(i.left)
-                        q1.append(i.right)
-                sum = 0
-                flag = 0
-                for i in q1:
-                    if i is not None:
-                        sum+=i.val
-                        flag = 1
-                if flag == 0:
-                    break
-                if sum>max_val:
-                    max_val = sum
-                    output = level + 1
-                q2.clear()
-                k = 1
-            level+=1
-        return output
+        max_sum, ans, level = float('-inf'), 0, 0
 
+        q = collections.deque()
+        q.append(root)
 
+        while q:
+            level += 1
+            sum_at_current_level = 0
+            # Iterate over all the nodes in the current level.
+            for _ in range(len(q)):
+                node = q.popleft()
+                sum_at_current_level += node.val
 
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
 
-
-
+            if max_sum < sum_at_current_level:
+                max_sum, ans = sum_at_current_level, level
+           
+        return ans
