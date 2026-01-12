58c8826976fc3eb8ecb8236b2e180a5412fa06dc
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2026-01-12 04:14:55 +0000

    [LeetCode Sync] Runtime - 4 ms (12.81%), Memory - 19.4 MB (25.51%)

--- Solutions/1395-minimum-time-visiting-all-points/solution.py
+++ Solutions/1395-minimum-time-visiting-all-points/solution.py
@@ -1,3 +1,20 @@
 class Solution:
     def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
-        return sum(max(abs(c0[1] - c1[1]), abs(c0[0] - c1[0])) for c0,c1 in pairwise(points))
+        if len(points) in [0,1]:
+            return 0
+        
+        res = 0
+
+        for i in range(1, len(points)):
+            x1,y1 = points[i]
+            x0,y0 = points[i-1]
+
+            dif_x = abs(x1-x0)
+            dif_y = abs(y1-y0)
+
+            min_dif = min(dif_x,dif_y)
+
+            res += min_dif + dif_x-min_dif + dif_y-min_dif
+
+        return res
+
