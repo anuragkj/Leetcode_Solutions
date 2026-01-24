9a1b411cf7c7929ad6c5b8b865944cb2577ad41b
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2026-01-24 19:22:57 +0000

    [LeetCode Sync] Runtime - 826 ms (89.22%), Memory - 34.3 MB (12.69%)

--- Solutions/1988-minimize-maximum-pair-sum-in-array/solution.py
+++ Solutions/1988-minimize-maximum-pair-sum-in-array/solution.py
@@ -1,7 +1,9 @@
 class Solution:
     def minPairSum(self, nums: List[int]) -> int:
         nums.sort()
-        res=0
-        for i in range(len(nums)//2):
-            res=max(res,nums[i]+nums[len(nums)-i-1])
+        res = 0
+
+        for i in range(len(nums) // 2):
+            res = max(res, nums[i] + nums[-1 - i])
+
         return res
