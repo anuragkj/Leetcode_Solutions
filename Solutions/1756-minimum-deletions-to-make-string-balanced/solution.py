5ddc835ae98cc432216ae4f91488fd57a8086b57
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2026-02-07 07:15:56 +0000

    [LeetCode Sync] Runtime - 428 ms (30.25%), Memory - 20.3 MB (41.48%)

--- Solutions/1756-minimum-deletions-to-make-string-balanced/solution.py
+++ Solutions/1756-minimum-deletions-to-make-string-balanced/solution.py
@@ -1,22 +1,16 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
-        cache = [[None]*2 for _ in range(len(s))]
-        
-        def dp(idx, b):
-            if idx >= len(s):
-                return 0
-            if cache[idx][b] is None:
-                if b:
-                    if s[idx] == 'a':
-                        res = 1 + dp(idx+1, b)
-                    else:
-                        res = dp(idx+1, b)
-                else:
-                    if s[idx] == 'b':
-                        res = min(1 + dp(idx+1, b), dp(idx+1, 1))
-                    else:
-                        res = dp(idx+1, b)
-                cache[idx][b] = res
-            return cache[idx][b]
-        
-        return dp(0, 0)
+        res = len(s)
+        a = 0
+        b = 0
+
+        for c in s:
+            a += (c == 'a')
+
+        for c in s:
+            a -= (c == 'a')
+            res = min(res, a + b)
+            b += (c == 'b')
+
+        return res
+
