2024899b8b42584d492c443809b7d8c487bd00b3
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2026-03-08 20:53:50 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (67.33%)

--- Solutions/2107-find-unique-binary-string/solution.py
+++ Solutions/2107-find-unique-binary-string/solution.py
@@ -1,3 +1,3 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        return ''.join('1' if nums[i][i] == '0' else '0' for i in range(len(nums)))
+        return "".join(chr(ord(x[i])^1) for i, x in enumerate(nums))
