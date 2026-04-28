a1163812a03e4c50951961870ce46bfbf6be7e72
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2026-04-28 00:19:04 +0000

    [LeetCode Sync] Runtime - 143 ms (58.52%), Memory - 40.8 MB (77.27%)

--- Solutions/2160-minimum-operations-to-make-a-uni-value-grid/solution.py
+++ Solutions/2160-minimum-operations-to-make-a-uni-value-grid/solution.py
@@ -1,13 +1,4 @@
 class Solution:
-    def minOperations(self, grid: List[List[int]], x: int) -> int:
-        values = sorted([val for row in grid for val in row])
-        
-        diff = [abs(val - values[0]) % x for val in values]
-        if any(d != 0 for d in diff):
-            return -1
-        
-        median = values[len(values) // 2]
-        operations = sum(abs(val - median) // x for val in values)
-        
-        return operations
-        
+    def minOperations(self, g: List[List[int]], x: int) -> int:
+        if any(abs(v-u)%x for v,u in pairwise(chain(*g))): return -1
+        return sum(map(abs,map(sub,chain(*g),repeat(median_low(chain(*g))))))//x
