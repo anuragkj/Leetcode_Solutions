10dc07c6af6b4bf33dbbf285e01d66a6a43bf711
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2026-05-03 17:47:35 +0000

    [LeetCode Sync] Runtime - 3 ms (6.02%), Memory - 19.5 MB (17.67%)

--- Solutions/0812-rotate-string/solution.py
+++ Solutions/0812-rotate-string/solution.py
@@ -1,6 +1,25 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        if len(s) != len(goal):
+        n = len(s)
+        if n != len(goal):
             return False
-        doubled_string = s + s
-        return doubled_string.find(goal) != -1
+
+        dfa = [[0] * 26 for _ in range(n)]
+        dfa[0][ord(s[0]) - 97] = 1
+
+        x = 0
+        for i in range(1, n):
+            for j in range(26):
+                dfa[i][j] = dfa[x][j]
+            c = ord(s[i]) - 97
+            dfa[i][c] = i + 1
+            x = dfa[x][c]
+
+        st = 0
+        for i in range(n << 1):
+            st = dfa[st][ord(goal[i % n]) - 97]
+            if st == n:
+                return True
+
+        return False
+
