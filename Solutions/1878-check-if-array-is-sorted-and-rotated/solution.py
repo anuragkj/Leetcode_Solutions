59bd7e81971b23c737da98019e025c2fc7c65eb9
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2026-05-23 19:43:05 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.1 MB (85.87%)

--- Solutions/1878-check-if-array-is-sorted-and-rotated/solution.py
+++ Solutions/1878-check-if-array-is-sorted-and-rotated/solution.py
@@ -1,14 +1,3 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
-        n = len(nums)
-        count = 1
-
-        for i in range(1, 2 * n):
-            if nums[(i - 1) % n] <= nums[i % n]:
-                count += 1
-            else:
-                count = 1
-            if count == n:
-                return True
-
-        return n == 1 # Handle edge case for single element
+        return sum(nums[i - 1] > nums[i] for i in range(len(nums))) < 2
